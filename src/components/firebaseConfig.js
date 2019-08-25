40305031e8d0d02fe417fd190cde7e4d92ac090e
eipapps <dleimmersion@gmail.com>
2019-08-25 01:21:09 -0400

    built for heroku

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,10 +1,20 @@
 export default{
-        apiKey: process.env.APIKEY,
-        authDomain: process.env.AUTHDOMAIN,
-        databaseURL: process.env.DATABASEURL,
+        apiKey: "AIzaSyAoJb6Mz39lqql1upLPVA9LN-wyYvlZujw",
+        authDomain: "teacheip.firebaseapp.com",
+        databaseURL: "https://teacheip.firebaseio.com",
         projectId: "teacheip",
-        storageBucket: process.env.STORAGEBUCKET,
-        messagingSenderId: process.env.MESSAGINGSENDERID,
-        appId: process.env.APPID 
+        storageBucket: "teacheip.appspot.com",
+        messagingSenderId: "890206241956",
+        appId: "1:890206241956:web:a4d3dbda7c089c08"
 }
+
+// export default{
+//         apiKey: process.env.APIKEY,
+//         authDomain: process.env.AUTHDOMAIN,
+//         databaseURL: process.env.DATABASEURL,
+//         projectId: "teacheip",
+//         storageBucket: process.env.STORAGEBUCKET,
+//         messagingSenderId: process.env.MESSAGINGSENDERID,
+//         appId: process.env.APPID 
+// }
 
