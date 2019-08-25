03b48acb61bd44c9d9a16c0cabc9b8deaf990edc
eipapps <dleimmersion@gmail.com>
2019-08-25 01:01:57 -0400

    built for heroku

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,10 +1,10 @@
 export default{
         apiKey: process.env.APIKEY,
-        authDomain: "process.env.AUTHDOMAIN",
-        databaseURL: "process.env.DATABASEURL",
-        projectId: "process.env.PROJECTID",
-        storageBucket: "process.env.STORAGEBUCKET",
-        messagingSenderId: "process.env.MESSAGINGSENDERID",
-        appId: "process.env.APPID" 
+        authDomain: process.env.AUTHDOMAIN,
+        databaseURL: process.env.DATABASEURL,
+        projectId: process.env.PROJECTID,
+        storageBucket: process.env.STORAGEBUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID 
 }
 
