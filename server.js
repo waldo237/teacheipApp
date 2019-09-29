d819a9338e6e09617c739dd2144cd070176c1697
eipapps <dleimmersion@gmail.com>
2019-09-29 01:00:11 -0400

    done

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const RateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 8080;
 const app = express(); 
+
+// rate limit setup
+const limiter = new RateLimit(
+    {
+        windowMs: 15*60*1000,
+        max:300,
+        delayMs:0
+    }
+)
 app.use(express.static(__dirname+ "/dist/"));
-app.get(/.*/, function(req, res){
+app.use(cookieParser());
+app.get(/.*/,  limiter, function(req, res){
     res.sendFile((__dirname+ "/dist/index.html"));
 });
 app.listen(port);
