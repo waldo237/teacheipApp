cbd2f2a35c5209963f61d18ab1214d107d6547e5
eipapps <dleimmersion@gmail.com>
2019-10-20 00:57:24 -0400

    aa

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,8 @@
-module.exports = {}
+// vue.config.js file to be place in the root of your repository
+// make sure you update `yourProjectName` with the name of your GitLab project
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/eipapp/'
+      : '/'
+  }
